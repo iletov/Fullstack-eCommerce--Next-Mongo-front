d94f70e9f5df5d9b4577b0276700b7146c8d2399
Ivan Letov <letov88@gmail.com>
2023-06-01 12:04:14 +0300

    mobile responsive

--- components/Colors.js
+++ components/Colors.js
@@ -1,3 +1,4 @@
 export const primary = '#5542f6';
 export const white = '#fff';
 export const black = '#000';
+export const dark = '#222';
--- components/Header.js
+++ components/Header.js
@@ -1,48 +1,98 @@
 import Link from 'next/link'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import Center from './Center'
 import { CartContext } from './CartContext'
+import Bars from './Bars'
+import { dark, white } from './Colors'
 
 const StyledHeader = styled.header`
-  background-color: #222;
-`
+  background-color: ${dark};
+  ${props => props.mobileView && `
+    position: fixed;
+    width: 100%;
+  `}
+`;
 
 const Logo = styled(Link)`
   color:#fff;
   text-decoration:none;
-`
+  position: relative;
+  z-index: 3;
+`;
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 20px 0;
-`
+`;
 const StyledNav = styled.nav`
-  display: flex;
+  ${props => props.mobileView ? `
+  display: block;
+  ` : `
+  display: none;
+  `}
   gap: 15px;
-`
+  position: fixed;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  padding: 50px 20px 20px;
+  background-color: ${dark};
+  @media screen and (min-width: 768px) {
+    display: flex;
+    position: static;
+    padding: 0;
+  }
+`;
 
 const NavLink = styled(Link)`
+  display: block;
   color:#aaa;
   text-decoration: none;
-`
+  padding: 10px 0;
+  @media screen and (min-width: 768px) {
+    padding: 0;
+  }
+`;
+
+const NavButton = styled.button`
+  background-color: transparent;
+  border: 0;
+  width: 40px;
+  height: 40px;
+  color: ${white};
+  cursor: pointer;
+  position: relative;
+  z-index: 3;
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
 
 const Header = () => {
   const {cartProducts} = useContext(CartContext);
+  const [mobileView, setMobileView] = useState(false);
 
   return (
-    <StyledHeader>
+    <StyledHeader mobileView={mobileView}>
       <Center>
         <Wrapper>
         <Logo href={'/'}>Ecommerce</Logo>
-          <StyledNav>
+
+          <StyledNav mobileView={mobileView}>
             <NavLink href={'/home'}>Home</NavLink>
             <NavLink href={'/products'}>All Products</NavLink>
             <NavLink href={'/categories'}>Categories</NavLink>
             <NavLink href={'/account'}>Account</NavLink>
             <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
           </StyledNav>
+
+          <NavButton onClick={() => setMobileView((prev) => !prev)}>
+            <Bars />
+          </NavButton>
+
         </Wrapper>
       </Center>
     </StyledHeader>
